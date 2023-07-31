ce881462f9ed5b9e0c4e637199da4acb5e869fe6
DimaCat <dimiiiitriy@gmail.com>
2023-07-31 21:40:15 +0300

    Задание 6-7

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        //Задание 5
-      var frog = 3.5;
-      System.out.println(frog);
-        frog = frog * 10;
-      System.out.println(frog);
-        frog = frog / 3.5;
-      System.out.println(frog);
-      frog = frog + 4;
-      System.out.print(frog);
+        //Задание 6
+      var boxer1 = 78.2;
+      var boxer2 = 82.7;
+      var answer1 = boxer1 + boxer2;
+      var answer2 = boxer2 - boxer1;
+      System.out.println("Боксеры весят вместе" +  answer1);
+      System.out.println("Разница в весе боксеров" + answer2);
+      // Задание 7
+        var asnwer3 = boxer2 % boxer1;
+        System.out.println("Остаток от деления" + asnwer3);
     }
 }
